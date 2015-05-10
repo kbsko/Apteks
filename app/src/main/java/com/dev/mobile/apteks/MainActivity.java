c5000e53950f34d888fcbd993b0ed2185aa3ad71
kbsko <aleksey-kb@mail.ru>
2015-05-10 08:39:04 +0700

    add default drug selector color

--- app/src/main/java/com/dev/mobile/apteks/MainActivity.java
+++ app/src/main/java/com/dev/mobile/apteks/MainActivity.java
@@ -5,18 +5,24 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    public static final String TAG="MyActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
+        Log.d(TAG, "onCreate()");
+        Log.d(TAG,"onCreate()");
+        Log.d(TAG,"onCreate()");
     }
 
 
